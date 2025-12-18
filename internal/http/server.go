0735020deeaaabaae0e0bc302d472940bf551815
dhernos <git@lwsnr.com>
2025-12-18 12:43:08 +0100

    deleted:    internal/http/fs.go 	modified:   internal/http/server.go 	internal/http/fs/ 	internal/http/stack/

--- internal/http/server.go
+++ internal/http/server.go
@@ -3,6 +3,8 @@ package http
 import (
 	"log"
 	"net/http"
+        "vestri-worker/internal/http/fs"
+        "vestri-worker/internal/http/stack"
 )
 
 func router() http.Handler {
@@ -10,9 +12,12 @@ func router() http.Handler {
 
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("/settings", settingsHandler)
-	mux.HandleFunc("/fs/read", readFileHandler)
-	mux.HandleFunc("/fs/write", writeFileHandler)
-
+	mux.HandleFunc("/fs/read", fs.ReadFileHandler)
+	mux.HandleFunc("/fs/write", fs.WriteFileHandler)
+        mux.HandleFunc("/stack/up", stack.StackUpHandler)
+        mux.HandleFunc("/stack/down", stack.StackDownHandler)
+        mux.HandleFunc("/stack/restart", stack.StackRestartHandler)
+        mux.HandleFunc("/stack/status", stack.StackStatusHandler)
 	return mux
 }
 

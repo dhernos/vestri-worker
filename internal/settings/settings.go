20c0a2826a2c01b04db52f9fe35706b0b9256741
dhernos <git@lwsnr.com>
2026-03-22 23:06:28 +0100

    release prep, cleanup, bug fixes

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -10,8 +10,6 @@ type Settings struct {
 	TLSSANs                []string `json:"tls_sans"`
 	HTTPPort               string   `json:"http_port"`
 	WorkerName             string   `json:"worker_name"`
-	LogLevel               string   `json:"log_level"`
-	MaxJobs                int      `json:"max_jobs"`
 	FsBasePath             string   `json:"fs_base_path"`
 	ReplayWindowSeconds    int      `json:"replay_window_seconds"`
 	RateLimitRPS           float64  `json:"rate_limit_rps"`
